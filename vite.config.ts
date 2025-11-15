50d204906715e7a185e31bc99a34c65cceaf1498
raobeid2025 <raobeid2024@gmail.com>
2025-11-15 12:18:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
+  base: "/3tilesGamev1/", // <--- Add this line for GitHub Pages
   server: {
     host: "::",
     port: 8080,
